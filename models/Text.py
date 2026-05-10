f9bc78c947066efcfbbcf41b6bb3e11dd1e9714f
userlowername <140051085+userlowername@users.noreply.github.com>
2026-05-10 15:17:04 +0800

    Add files via upload

---
+++ models/Text.py
@@ -0,0 +1,154 @@
+####################################### 2 Classes #######################################
+class_names_2 = [
+'real.',
+'fake.'
+]
+class_names_with_context_2 = [
+'a photo of a real face.',
+'a photo of a fake face.'
+]
+class_descriptor_2 = [
+'genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'blur mouth,blur eyes,blur outline,blur nose,blur eyebrows,dim mouth,abnormal eyes,unclear outline,asymmetry eyebrows,asymmetry eyes,abnormal structure.'
+]
+class_names_6 = [
+'realpostive.',
+'realnegative.',
+'realneutral.',
+'fakepostive.',
+'fakenegative.',
+'fakeneutral.'
+]
+class_names_with_context_6 = [
+'an expression of realpostive.',
+'an expression of realnegative.',
+'an expression of realneutral.',
+'an expression of fakepostive.',
+'an expression of fakenegative.',
+'an expression of fakeneutral.'
+]
+class_descriptor_6 = [
+'a smiling mouth, raised cheeks,genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'drooping upper eyelids, a retracted chin,genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'relaxed facial muscles, a straight mouth,genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'wrinkled eyes, and arched eyebrows,blur mouth,blur eyes,blur nose,blur eyebrows,dim mouth,unclear outline,asymmetry eyes.',
+'a downward turned mouth,a wrinkled forehead,blur mouth,blur eyes,blur nose,blur eyebrows,dim mouth,unclear outline,asymmetry eyes.',
+'a smooth forehead, and unremarkable eyebrows,blur mouth,blur eyes,blur nose,blur eyebrows,dim mouth,unclear outline,asymmetry eyes.',
+]
+####################################### 7 Classes #######################################
+class_names_7 = [
+'realhappiness.',
+'realsadness.',
+'realneutral.',
+'realanger.',
+'realsurprise.',
+'realdisgust.',
+'realfear.'
+]
+class_names_with_context_7 = [
+'an expression of happiness.',
+'an expression of sadness.',
+'an expression of neutral.',
+'an expression of anger.',
+'an expression of surprise.',
+'an expression of disgust.',
+'an expression of fear.'
+]
+class_descriptor_7 = [
+'a smiling mouth, raised cheeks, wrinkled eyes, and arched eyebrows.',
+'tears, a downward turned mouth, drooping upper eyelids, and a wrinkled forehead.',
+'relaxed facial muscles, a straight mouth, a smooth forehead, and unremarkable eyebrows.',
+'furrowed eyebrows, narrow eyes, tightened lips, and flared nostrils.',
+'widened eyes, an open mouth, raised eyebrows, and a frozen expression.',
+'a wrinkled nose, lowered eyebrows, a tightened mouth, and narrow eyes.',
+'raised eyebrows, parted lips, a furrowed brow, and a retracted chin.',
+]
+
+####################################### 11 Classes #######################################
+class_names_11 = [
+'happiness.',
+'sadness.',
+'neutral.',
+'anger.',
+'surprise.',
+'disgust.',
+'fear.',
+'contempt.',
+'anxiety.',
+'helplessness.',
+'disappointment.'
+]
+class_names_with_context_11 = [
+'an expression of happiness.',
+'an expression of sadness.',
+'an expression of neutral.',
+'an expression of anger.',
+'an expression of surprise.',
+'an expression of disgust.',
+'an expression of fear.',
+'an expression of contempt.',
+'an expression of anxiety.',
+'an expression of helplessness.',
+'an expression of disappointment.',
+]
+class_descriptor_11 = [
+'a smiling mouth, raised cheeks, wrinkled eyes, and arched eyebrows.',
+'tears, a downward turned mouth, drooping upper eyelids, and a wrinkled forehead.',
+'relaxed facial muscles, a straight mouth, a smooth forehead, and unremarkable eyebrows.',
+'furrowed eyebrows, narrow eyes, tightened lips, and flared nostrils.',
+'widened eyes, an open mouth, raised eyebrows, and a frozen expression.',
+'a wrinkled nose, lowered eyebrows, a tightened mouth, and narrow eyes.',
+'raised eyebrows, parted lips, a furrowed brow, and a retracted chin.',
+'one side of its mouth raised, one eyebrow lower and one raised, narrowed eyes, and a raised chin.',
+'a tensed forehead, tightly pressed lips, pupil dilation, and tensed facial muscles.',
+'drooping eyebrows, a downward gaze, a downturned mouth, and lacking expression.',
+'a downturned mouth, lowered eyebrows, narrowed eyes, and a sighing face.'
+]
+class_names_14 = [
+'realhappiness.',
+'realsadness.',
+'realneutral.',
+'realanger.',
+'realsurprise.',
+'realdisgust.',
+'realfear.',
+'fakehappiness.',
+'fakesadness.',
+'fakeneutral.',
+'fakeanger.',
+'fakesurprise.',
+'fakedisgust.',
+'fakefear.'
+]
+class_names_with_context_14 = [
+'an expression of realhappiness.',
+'an expression of realsadness.',
+'an expression of realneutral.',
+'an expression of realanger.',
+'an expression of realsurprise.',
+'an expression of realdisgust.',
+'an expression of realfear.',
+'an expression of fakehappiness.',
+'an expression of fakesadness.',
+'an expression of fakeneutral.',
+'an expression of fakeanger.',
+'an expression of fakesurprise.',
+'an expression of fakedisgust.',
+'an expression of fakefear.'
+]
+class_descriptor_14 = [
+'a smiling mouth, raised cheeks, wrinkled eyes, and arched eyebrows,genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'tears, a downward turned mouth, drooping upper eyelids, and a wrinkled forehead,genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'relaxed facial muscles, a straight mouth, a smooth forehead, and unremarkable eyebrows,genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'furrowed eyebrows, narrow eyes, tightened lips, and flared nostrils,genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'widened eyes, an open mouth, raised eyebrows, and a frozen expression,genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'a wrinkled nose, lowered eyebrows, a tightened mouth, and narrow eyes.genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'raised eyebrows, parted lips, a furrowed brow, and a retracted chin,genuine mouth,twinkling eyes,clear outline,symmetry eyebrows,clear nose.',
+'a smiling mouth, raised cheeks, wrinkled eyes, and arched eyebrows,blur mouth,blur eyes,blur outline,blur nose,blur eyebrows,dim mouth,abnormal eyes,unclear outline,asymmetry eyebrows,asymmetry eyes,abnormal nose, abnormal mouth.',
+'tears, a downward turned mouth, drooping upper eyelids, and a wrinkled forehead,blur mouth,blur eyes,blur outline,blur nose,blur eyebrows,dim mouth,abnormal eyes,unclear outline,asymmetry eyebrows,asymmetry eyes,abnormal structure.',
+'relaxed facial muscles, a straight mouth, a smooth forehead, and unremarkable eyebrows,fblur mouth,blur eyes,blur outline,blur nose,blur eyebrows,dim mouth,abnormal eyes,unclear outline,asymmetry eyebrows,asymmetry eyes,abnormal structure.',
+'furrowed eyebrows, narrow eyes, tightened lips, and flared nostrils,blur mouth,blur eyes,blur outline,blur nose,blur eyebrows,dim mouth,abnormal eyes,unclear outline,asymmetry eyebrows,asymmetry eyes,abnormal structure.',
+'widened eyes, an open mouth, raised eyebrows, and a frozen expression,blur mouth,blur eyes,blur outline,blur nose,blur eyebrows,dim mouth,abnormal eyes,unclear outline,asymmetry eyebrows,asymmetry eyes,abnormal structure.',
+'a wrinkled nose, lowered eyebrows, a tightened mouth, and narrow eyes,blur mouth,blur eyes,blur outline,blur nose,blur eyebrows,dim mouth,abnormal eyes,unclear outline,asymmetry eyebrows,asymmetry eyes,abnormal structure.',
+'raised eyebrows, parted lips, a furrowed brow, and a retracted chin,blur mouth,blur eyes,blur outline,blur nose,blur eyebrows,dim mouth,abnormal eyes,unclear outline,asymmetry eyebrows,asymmetry eyes,abnormal structure.',
+]
